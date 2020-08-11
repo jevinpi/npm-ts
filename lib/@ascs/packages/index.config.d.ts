0f875a59958bb4d7ca3898ee382583c1f181dba5
皮健云 <pijianyun@ascs.tech>
2020-08-11 15:56:13 +0800

    feat: INITIAL COMMIT

---
+++ lib/@ascs/packages/index.config.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Interface
+ */
+export interface IIndex {
+    name: string;
+    age: number;
+}
+export interface IExample {
+    name: string;
+    age: number;
+}
--- lib/@ascs/packages/index.d.ts
+++ lib/@ascs/packages/index.d.ts
@@ -1,27 +1,8 @@
-interface IMinioConfig {
-    endPoint: string;
-    port: number;
-    useSSL: boolean;
-    accessKey: string;
-    secretKey: string;
-    bucketName: string;
+import { IIndex, IExample } from './index.config';
+declare class Example implements IExample {
+    name: string;
+    age: number;
+    constructor(name?: string, age?: number);
+    fn(): IIndex;
 }
-interface IUploader {
-    config: IMinioConfig;
-    Client: any;
-    readFile: Function;
-    upload: Function;
-    delete: Function;
-}
-declare class Uploader implements IUploader {
-    config: IMinioConfig;
-    Client: any;
-    constructor(option?: Partial<IMinioConfig>);
-    readFile(file: File): Promise<string>;
-    setBucketPolicy(): any;
-    bucketExists(): Promise<any>;
-    getFile(fileUrl: string): any;
-    upload(file: any): Promise<any>;
-    delete(): void;
-}
-export default Uploader;
+export default Example;
